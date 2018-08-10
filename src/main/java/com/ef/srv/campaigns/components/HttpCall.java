ad04b14e23bf4b9980a57dfdd41ea6a822bf4888
carlos asensio <carlos.asensio@grupoonetec.com>
2018-08-10 12:41:05 +0200

    Se ha refactorizado código

--- src/main/java/com/ef/srv/campaigns/components/HttpCall.java
+++ src/main/java/com/ef/srv/campaigns/components/HttpCall.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Properties;
